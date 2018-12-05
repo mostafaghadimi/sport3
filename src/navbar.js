037d6ebe61c92688b969b1f6cd024261d6af2e0f
unknown <mostafa.ghadimi@yahoo.com>
2018-12-06 02:17:47 +0330

    resolve responsive navbar bugs

--- src/navbar.js
+++ src/navbar.js
@@ -1,54 +1,86 @@
 import React, {Component} from 'react';
 import './assets/css/navbar.css';
-import './assets/js/navbar.js'
+// import './assets/js/navbar.js'
 
 
 
 class Navbar extends Component {
+    constructor(props){
+        super(props);
+        this.menuIcon = this.menuIcon.bind(this)
+    }
+
+    menuIcon (){
+        let nav = document.querySelector('.overlay-menu');
+        if (nav.style.transform != 'translateX(0%)') {
+            nav.style.transform = 'translateX(0%)';
+            nav.style.transition = 'transform 0.2s ease-out';
+        } else { 
+            nav.style.transform = 'translateX(100%)';
+            nav.style.transition = 'transform 0.2s ease-out';
+        }
+        let toggleIcon = document.querySelector('.menuIcon');
+
+        if (toggleIcon.className != 'menuIcon toggle') {
+            toggleIcon.className += ' toggle';
+        } else {
+            toggleIcon.className = 'menuIcon';
+        }
+    }
     render(){
         return (
             <div>
                 <nav id="navbar">
                     <div className="nav-wrapper">
                         <div className="logo">
-                        Sport3
+                            Sport3
                         </div>
-
-                        
                         <ul id="menu">
-                        <li><span className="football"></span><a href="#" >فوتبال</a></li>
-                        <li className="basketball"><a href="#" >بسکتبال</a></li>
+                            <li>
+                                <a href="#" >
+                                فوتبال
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    بسکتبال
+                                </a>
+                            </li>
                         </ul>
-
                         <div className="search">
-                        <ul>
-                            <li>
-                                <input type="search" placeholder="جستجو..."/>
-                            </li>
-                            <li>
-                                <span className="loginSignup"></span>
-                            </li>
-                        </ul>
+                            <ul>
+                                <li>
+                                    <input type="search" placeholder="جستجو..."/>
+                                </li>
+                                <li>
+                                    <span className="loginSignup"></span>
+                                </li>
+                            </ul>
                         </div>
                     </div>
-                    </nav>
-
-
-                    
-                    <div className="menuIcon">
+                </nav>                    
+                <div className="menuIcon" onClick={this.menuIcon}>
                     <span className="icon icon-bars"></span>
                     <span className="icon icon-bars overlay"></span>
-                    </div>
-
-
-                    <div className="overlay-menu">
-                    <ul id="menu">
-                        <li className="football"><a href="#">فوتبال</a></li>
-                        <li className="basketball"><a href="#">بسکتبال</a></li>
-                        </ul>
-                    </div>
                 </div>
+
+                <div className="overlay-menu">
+                    <ul id="menu">
+                        <li>
+                            <a href="#">
+                            فوتبال
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                            بسکتبال
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         )
+        
     }
 }
 
