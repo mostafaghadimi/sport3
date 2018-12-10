0276348234f343a6de50db63ed233881d32b57c4
unknown <mostafa.ghadimi@yahoo.com>
2018-12-11 00:22:57 +0330

    match statistics and ball possession have been added

--- src/footballMatch.js
+++ src/footballMatch.js
@@ -114,6 +114,61 @@ class FootballMatch extends Component {
                         </div>
                     </div>
                 </div>
+                <div className="footballMatchStatistics">
+                    <div className="footballMatchStatisticsTitle">آمار بازی</div>
+                    <div className="footballMatchStats">
+                        <div className="footballMatchStatsDetail">
+                            <div className="matchDetailFirstTeamLogo">
+                                <img src={require('./assets/img/manchester-united.png')} />
+                                <span>منچستر...</span>
+                            </div>
+                            <div>
+                            </div>
+                            <div className="matchDetailSecondTeamLogo">
+                                <span>فولهام</span>
+                                <img src={require('./assets/img/fulham.png')} />
+                            </div>
+
+                            <div>11</div>
+                            <div>خطا</div>
+                            <div>15</div>
+
+                            <div>1</div>
+                            <div>کارت زرد</div>
+                            <div>1</div>
+
+                            <div>0</div>
+                            <div>کارت قرمز</div>
+                            <div>1</div>
+
+                            <div>4</div>
+                            <div>آفساید</div>
+                            <div>3</div>
+
+                            <div>10</div>
+                            <div>کرنر</div>
+                            <div>3</div>
+                        </div>
+                        <div className="footballMatchBallPoss">
+                            <div className="footballMatchFirstTeamPoss">
+                                <img src={require('./assets/img/manchester-united.png')} />
+                            </div>
+                            <div className="footballMatchPoss">مالکیت توپ</div>
+                            <div className="footballMatchSecondTeamPoss">
+                                <img src={require('./assets/img/fulham.png')} />
+                            </div>
+                            <div>
+                                %63
+                            </div>
+                            <div>
+                                <progress value="63" max="100"></progress>
+                            </div>
+                            <div>
+                                %37
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
     
         )
